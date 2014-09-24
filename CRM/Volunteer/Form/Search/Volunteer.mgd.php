ce985e8095c2f4ace1ce723bb70f49e3a0d32ab3
Frank J. Gómez <frank@ginkgostreet.com>
2014-09-24 17:19:24 -0400

    VOL-51: Created custom search boilerplate.

---
+++ CRM/Volunteer/Form/Search/Volunteer.mgd.php
@@ -0,0 +1,19 @@
+<?php
+// This file declares a managed database record of type "CustomSearch".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Volunteer_Form_Search_Volunteer',
+    'entity' => 'CustomSearch',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'Volunteer',
+      'description' => 'Volunteer (org.civicrm.volunteer)',
+      'class_name' => 'CRM_Volunteer_Form_Search_Volunteer',
+    ),
+  ),
+);
